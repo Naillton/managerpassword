f8699ae4799d78204b17fb09005d847312c1f0f4
Naillton <nailton_junior@protonmail.com>
2023-11-29 10:20:00 -0300

    Config structure project, docker and set application properties

---
+++ src/main/java/com/nailton/managerpassword/ManagerpasswordApplication.java
@@ -0,0 +1,13 @@
+package com.nailton.managerpassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerpasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerpasswordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nailton/managerpassword/ManagerpasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nailton.managerpassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerpasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
